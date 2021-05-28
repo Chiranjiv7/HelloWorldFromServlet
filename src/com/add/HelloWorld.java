812e7cada67117afbafad7e09b8334c565ded0b5
Chiranjiv Bhatewara <Chiranjivbhatewara@gmail.com>
2021-05-28 14:55:54 +0530

    Servlet Program

---
+++ src/com/add/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.add;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{
+		res.getWriter().println("HelloWorldFromServlet");
+	}
+
+}
